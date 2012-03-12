0c4d40fc8f64d0a8b2cae680b20005929ac6317a
rjerome <rjerome@localhost>
2012-03-12 10:13:53 -0400

    changed module_path definition in csl_locale class to dirname(__FILE__);

--- CiteProc.php
+++ CiteProc.php
@@ -1402,7 +1402,7 @@ class csl_locale  {
   private   $module_path;
 
   function __construct($lang = 'en') {
-    $this->module_path = '.';
+    $this->module_path = dirname(__FILE__);
     $this->locale = new SimpleXMLElement($this->get_locales_file_name($lang));
     if ($this->locale) {
       $this->locale->registerXPathNamespace('cs', 'http://purl.org/net/xbiblio/csl');
