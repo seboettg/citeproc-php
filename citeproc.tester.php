4e5a6fc79c7365018cd5c7fa6872b1c5e01f19d9
rjerome <rjerome@localhost>
2012-12-06 21:21:10 -0500

    more updates to test framework Test "result" data still needs to be modified since the font styling is done differently and this causes false test failures

--- citeproc.tester.php
+++ citeproc.tester.php
@@ -5,9 +5,33 @@ $dir_handle = opendir('./tests');
 while (FALSE !== ($filename = readdir($dir_handle))) {
   if ( !is_dir('./tests/' . $filename) && $filename[0] != '.') {
     $json_data = file_get_contents('./tests/' . $filename);
-    $json_data = substr($json_data, strpos($json_data, '*/{')+2);
+  //  $json_data = substr($json_data, strpos($json_data, '*/{')+2);
     $test_data = json_decode($json_data);
-    if ($test_data->mode != 'bibliography') {
+      switch (json_last_error()) {
+        case JSON_ERROR_NONE:
+            echo ' - No errors';
+        break;
+        case JSON_ERROR_DEPTH:
+            echo ' - Maximum stack depth exceeded';
+        break;
+        case JSON_ERROR_STATE_MISMATCH:
+            echo ' - Underflow or the modes mismatch';
+        break;
+        case JSON_ERROR_CTRL_CHAR:
+            echo ' - Unexpected control character found';
+        break;
+        case JSON_ERROR_SYNTAX:
+            echo ' - Syntax error, malformed JSON';
+        break;
+        case JSON_ERROR_UTF8:
+            echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+        break;
+        default:
+            echo ' - Unknown error';
+        break;
+    }
+
+    if ($test_data->mode == 'bibliography') {
       $citeproc = new citeproc($test_data->csl);
       $input_data  = (array)$test_data->input;
       $count =  count($input_data);
