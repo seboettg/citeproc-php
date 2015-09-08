58926b3d7b0c9b38042101deca31195df7e660aa
Sebastian Böttger <seboettg@users.noreply.github.com>
2015-09-08 18:37:06 +0200

    function_exists

--- vendorPath.php
+++ vendorPath.php
@@ -26,17 +26,22 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/**
- * @return bool|string returns path of composer vendor folder, and false if folder does not exist
- */
-function vendorPath() {
-    if (file_exists(__DIR__.'/vendor/')) {
-        return __DIR__.'/vendor';
-    } else {
-        if (!file_exists(__DIR__.'/../../')) {
-            return false;
-        } else {
-            return __DIR__.'/../..';
-        }
-    }
+
+if (!function_exists('vendorPath')) {
+
+	/**
+	 * @return bool|string returns path of composer vendor folder, and false if folder does not exist
+	 */
+	function vendorPath() {
+
+		if (file_exists(__DIR__ . '/vendor/')) {
+			return __DIR__ . '/vendor';
+		} else {
+			if (!file_exists(__DIR__ . '/../../')) {
+				return false;
+			} else {
+				return __DIR__ . '/../..';
+			}
+		}
+	}
 }
