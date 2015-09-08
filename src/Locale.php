1f9de58883dd6b9d068811782eed732663625b31
Sebastian Böttger <seboettg@users.noreply.github.com>
2015-09-08 17:33:23 +0200

    fixes error in paths and constructor citeproc init

--- src/Locale.php
+++ src/Locale.php
@@ -109,9 +109,9 @@ class Locale {
         );
         
         if(isset($lang_bases[$lang])) {
-            $locale_file = file_get_contents($this->module_path . '/../vendor/citation-style-language/locales/locales-' . $lang_bases[$lang] . '.xml');
+            $locale_file = file_get_contents($this->module_path . '/../vendor/academicpuma/locales/locales-' . $lang_bases[$lang] . '.xml');
         } else {
-            $locale_file = file_get_contents($this->module_path . '/../vendor/citation-style-language/locales/locales-en-US.xml');
+            $locale_file = file_get_contents($this->module_path . '/../vendor/academicpuma/locales/locales-en-US.xml');
         }
         
         return $locale_file;
