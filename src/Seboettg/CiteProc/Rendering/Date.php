1cbbe26f2e726e895a2a1b289cdc1286f03c0fe4
Sebastian Böttger <seboettg@gmail.com>
2016-08-21 17:08:55 +0200

    Dummy classes added

---
+++ src/Seboettg/CiteProc/Rendering/Date.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Seboettg\CiteProc\Rendering;
+
+
+/**
+ * Class Date
+ * @package Seboettg\CiteProc\Rendering
+ *
+ * @author Sebastian Böttger <boettger@hebis.uni-frankfurt.de>
+ */
+class Date
+{
+
+}
