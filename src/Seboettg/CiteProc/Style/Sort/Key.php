ff9bb990c388177300d970abd75e0fbe1e52a26b
Sebastian Böttger <seboettg@gmail.com>
2016-08-29 16:16:51 +0200

    sorting

---
+++ src/Seboettg/CiteProc/Style/Sort/Key.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Seboettg\CiteProc\Style\Sort;
+
+
+/**
+ * Class Key
+ * @package Seboettg\CiteProc\Style\Sort
+ *
+ * @author Sebastian Böttger <boettger@hebis.uni-frankfurt.de>
+ */
+class Key
+{
+
+}
