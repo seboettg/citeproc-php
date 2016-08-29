93a70e0fd3bbd4d768d1d207ec4f12719fd9b454
Sebastian Böttger <seboettg@gmail.com>
2016-08-29 16:15:16 +0200

    affixes/quotes/range-delimiter

--- src/Seboettg/CiteProc/Styles/AffixesTrait.php
+++ src/Seboettg/CiteProc/Styles/AffixesTrait.php
@@ -2,6 +2,7 @@
 
 namespace Seboettg\CiteProc\Styles;
 
+use Seboettg\CiteProc\CiteProc;
 
 trait AffixesTrait
 {
@@ -10,8 +11,11 @@ trait AffixesTrait
 
     private $suffix = "";
 
+    private $quotes = false;
+
     protected function initAffixesAttributes(\SimpleXMLElement $node)
     {
+        /** @var \SimpleXMLElement $attribute */
         foreach ($node->attributes() as $attribute) {
             /** @var string $name */
             $name = (string)$attribute->getName();
@@ -24,12 +28,38 @@ trait AffixesTrait
                 case 'suffix':
                     $this->suffix = $value;
                     break;
+                case 'quote':
+                    $this->quotes = (bool) $attribute;
             }
         }
     }
 
     protected function addAffixes($text)
     {
-        return $this->prefix . $text . $this->suffix;
+        if ($this->quotes) {
+            $openQuotes = CiteProc::getContext()->getLocale()->filter("terms", "open-quote")->single;
+            $closeQuote = CiteProc::getContext()->getLocale()->filter("terms", "close-quote")->sinlge;
+            $punctuationInQuote = CiteProc::getContext()->getLocale()->filter("terms", "punctuation-in-quote")->single;
+        }
+
+
+        $prefix = $this->prefix;
+        $prefix .= isset($openQuotes) ? $openQuotes : '';
+        $suffix = $this->suffix;
+        if (isset($closeQuote) && !empty($suffix) && isset($punctuationInQuote)) {
+            if (strpos($suffix, '.') !== false || strpos($suffix, ',') !== false) {
+                $suffix = $suffix . $closeQuote;
+            }
+        } elseif (isset($closeQuote)) {
+            $suffix = $closeQuote . $suffix;
+        }
+        if (!empty($suffix)) { // guard against repeated suffixes...
+            $no_tags = strip_tags($text);
+            if (strlen($no_tags) && ($no_tags{(strlen($no_tags) - 1)} == $suffix{0})) {
+                $suffix = substr($suffix, 1);
+            }
+        }
+
+        return $prefix . $text . $suffix;
     }
 }
