529ee9e1d473880aa24afbe9f3763c760e42a4dd
Sebastian Böttger <seboettg@gmail.com>
2016-08-30 14:02:49 +0200

    fixed name rendering, Et-al abbreviation output, additional test cases

--- tests/Seboettg/CiteProc/Rendering/Name/NamesTest.php
+++ tests/Seboettg/CiteProc/Rendering/Name/NamesTest.php
@@ -53,14 +53,17 @@ class NamesTest extends \PHPUnit_Framework_TestCase
 
         $names = new Names(new \SimpleXMLElement($xml));
         $ret = $names->render(json_decode($data));
-        $this->assertEquals("John la Doe, Hrsg.", $ret);
+        $this->assertEquals("J. de la Doe, Hrsg.", $ret);
 
+        $xml = "<names variable=\"editor\" delimiter=\", \"><name and=\"symbol\" delimiter=\", \"/><label form=\"short\" prefix=\", \" text-case=\"title\"/></names>";
+        $names = new Names(new \SimpleXMLElement($xml));
+        $ret = $names->render(json_decode($data));
+        $this->assertEquals("John de la Doe, Hrsg.", $ret);
     }
 
     public function testRenderAuthorAndEditor()
     {
         $data = "{\"author\": [{\"dropping-particle\": \"\", \"family\": \"Einstein\", \"given\": \"Albert\", \"non-dropping-particle\": \"\"}],\"editor\": [{\"dropping-particle\": \"de\", \"family\": \"Doe\", \"given\": \"John\", \"non-dropping-particle\": \"la\", \"static-ordering\": false}], \"id\": \"ITEM-1\", \"title\": \"His Anonymous Life\", \"type\": \"book\"}";
-
 
         $xml = "<group>
                     <names variable=\"author\" delimiter=\"; \" suffix=\" in: \">
@@ -68,31 +71,48 @@ class NamesTest extends \PHPUnit_Framework_TestCase
                     </names>
                     <text variable=\"title\" text-case=\"title\" suffix=\", \"/>
                     <names variable=\"editor\" delimiter=\", \" prefix=\"(\" suffix=\")\">
-                        <name and=\"symbol\" initialize-with=\". \" delimiter=\", \"/>
+                        <name and=\"symbol\" name-as-sort-order=\"all\" initialize-with=\". \" delimiter=\", \"/>
                         <label form=\"short\" prefix=\", \" text-case=\"title\"/>
                     </names>
                 </group>";
 
         $group = new Group(new \SimpleXMLElement($xml));
         $ret = $group->render(json_decode($data));
-        $this->assertEquals("Einstein, Albert in: His Anonymous Life, (John la Doe, Hrsg.)", $ret);
+        $this->assertEquals("Einstein, Albert in: His Anonymous Life, (la Doe, J. de, Hrsg.)", $ret);
 
         //two authors
         $data2 = "{\"author\": [{\"dropping-particle\": \"\", \"family\": \"Einstein\", \"given\": \"Albert\", \"non-dropping-particle\": \"\"}, {\"dropping-particle\": \"\", \"family\": \"Skłodowska Curie\", \"given\": \"Marie\", \"non-dropping-particle\": \"\"}],\"editor\": [{\"dropping-particle\": \"de\", \"family\": \"Doe\", \"given\": \"John\", \"non-dropping-particle\": \"la\", \"static-ordering\": false}], \"id\": \"ITEM-1\", \"title\": \"Das Leben des Brian\", \"type\": \"book\"}";
         $ret2 = $group->render(json_decode($data2));
-        $this->assertEquals("Einstein, Albert; Skłodowska Curie, Marie in: Das Leben Des Brian, (John la Doe, Hrsg.)", $ret2);
+        $this->assertEquals("Einstein, Albert; Skłodowska Curie, Marie in: Das Leben Des Brian, (la Doe, J. de, Hrsg.)", $ret2);
 
     }
 
     public function testRenderMultipleAuthors()
     {
-        $data = "{\"author\": [{\"dropping-particle\": \"de\", \"family\": \"Doe\", \"given\": \"John\", \"non-dropping-particle\": \"la\", \"static-ordering\": false}, {\"dropping-particle\": \"\", \"family\": \"Doe\", \"given\": \"Jane\", \"non-dropping-particle\": \"\", \"static-ordering\": false}], \"id\": \"ITEM-1\", \"title\": \"Her Anonymous Life\", \"type\": \"book\"}";
-
-        $xml = "<names variable=\"author\" delimiter=\", \" prefix=\" (\" suffix=\")\"><name and=\"symbol\" initialize-with=\". \" delimiter=\", \"/><label form=\"short\" prefix=\", \" text-case=\"title\"/></names>";
-
+        $xml = "<names variable=\"author\" name-as-sort-order=\"all\" delimiter=\", \" prefix=\"(\" suffix=\")\"><name form=\"short\" and=\"symbol\" delimiter=\", \"/><label form=\"short\" prefix=\", \" text-case=\"title\"/></names>";
+        echo "$xml\n";
         $names = new Names(new \SimpleXMLElement($xml));
 
-        $ret = $names->render(json_decode($data));
-        $x = "foo";
+        // two names
+        $data = "{\"author\": [{\"dropping-particle\": \"de\", \"family\": \"Doe\", \"given\": \"John\", \"non-dropping-particle\": \"la\", \"static-ordering\": false}, {\"dropping-particle\": \"\", \"family\": \"Curie\", \"given\": \"Marie\", \"non-dropping-particle\": \"\", \"static-ordering\": false}], \"id\": \"ITEM-1\", \"title\": \"Her Anonymous Life\", \"type\": \"book\"}";
+        $this->assertEquals("(la Doe & Curie)", $names->render(json_decode($data)));
+
+        // three names
+        $data = "{\"author\": [{\"dropping-particle\": \"de\", \"family\": \"Doe\", \"given\": \"John\", \"non-dropping-particle\": \"la\", \"static-ordering\": false}, {\"dropping-particle\": \"\", \"family\": \"Curie\", \"given\": \"Marie\", \"non-dropping-particle\": \"\", \"static-ordering\": false}, {\"dropping-particle\": \"\", \"family\": \"Einstein\", \"given\": \"Albert\", \"non-dropping-particle\": \"\", \"static-ordering\": false}], \"id\": \"ITEM-1\", \"title\": \"Her Anonymous Life\", \"type\": \"book\"}";
+        $this->assertEquals("(la Doe, Curie, & Einstein)", $names->render(json_decode($data)));
+
+        // four names
+        $data = "{\"author\": [{\"dropping-particle\": \"de\", \"family\": \"Doe\", \"given\": \"John\", \"non-dropping-particle\": \"la\", \"static-ordering\": false}, {\"dropping-particle\": \"\", \"family\": \"Curie\", \"given\": \"Marie\", \"non-dropping-particle\": \"\", \"static-ordering\": false}, {\"dropping-particle\": \"\", \"family\": \"Einstein\", \"given\": \"Albert\", \"non-dropping-particle\": \"\", \"static-ordering\": false}, {\"dropping-particle\": \"\", \"family\": \"Röntgen\", \"given\": \"Wilhelm Conrad\", \"non-dropping-particle\": \"\", \"static-ordering\": false}], \"id\": \"ITEM-1\", \"title\": \"Her Anonymous Life\", \"type\": \"book\"}";
+        $this->assertEquals("(la Doe, Curie, Einstein, & Röntgen)", $names->render(json_decode($data)));
+    }
+
+    public function testRenderMultipleAuthorsEtAl()
+    {
+        $xml = "<names variable=\"author\" name-as-sort-order=\"all\" delimiter=\", \" prefix=\"(\" suffix=\")\"><name form=\"short\" and=\"symbol\" delimiter=\", \" et-al-min=\"4\" et-al-use-first=\"2\"/></names>";
+        $names = new Names(new \SimpleXMLElement($xml));
+
+        // four names
+        $data = "{\"author\": [{\"dropping-particle\": \"de\", \"family\": \"Doe\", \"given\": \"John\", \"non-dropping-particle\": \"la\", \"static-ordering\": false}, {\"dropping-particle\": \"\", \"family\": \"Curie\", \"given\": \"Marie\", \"non-dropping-particle\": \"\", \"static-ordering\": false}, {\"dropping-particle\": \"\", \"family\": \"Einstein\", \"given\": \"Albert\", \"non-dropping-particle\": \"\", \"static-ordering\": false}, {\"dropping-particle\": \"\", \"family\": \"Röntgen\", \"given\": \"Wilhelm Conrad\", \"non-dropping-particle\": \"\", \"static-ordering\": false}], \"id\": \"ITEM-1\", \"title\": \"Her Anonymous Life\", \"type\": \"book\"}";
+        $this->assertEquals("(la Doe, Curie, u. a.)", $names->render(json_decode($data)));
     }
 }
