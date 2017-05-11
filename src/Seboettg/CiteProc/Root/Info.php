764d24f7fa0fc186089cdf69467bd212e3e9193f
Sebastian Böttger <boettger@hebis.uni-frankfurt.de>
2017-05-11 08:02:01 +0200

    parse style info node, bugfix inheritable name delimiter, code formatting

--- src/Seboettg/CiteProc/Root/Info.php
+++ src/Seboettg/CiteProc/Root/Info.php
@@ -78,27 +78,11 @@ class Info
     }
 
     /**
-     * @param string $title
-     */
-    public function setTitle($title)
-    {
-        $this->title = $title;
-    }
-
-    /**
      * @return string
      */
     public function getId()
     {
         return $this->id;
-    }
-
-    /**
-     * @param string $id
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
     }
 
     /**
@@ -110,26 +94,10 @@ class Info
     }
 
     /**
-     * @param array $authors
-     */
-    public function setAuthors($authors)
-    {
-        $this->authors = $authors;
-    }
-
-    /**
      * @return array
      */
     public function getLinks()
     {
         return $this->links;
-    }
-
-    /**
-     * @param array $links
-     */
-    public function setLinks($links)
-    {
-        $this->links = $links;
     }
 }
