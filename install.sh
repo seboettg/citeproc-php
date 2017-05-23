b494429c523de8667a682141e8b8be585f945ce8
Sebastian Böttger <boettger@hebis.uni-frankfurt.de>
2017-05-23 15:44:00 +0200

    composer

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 if [ -d ./vendor/citation-style-language/$1 ]
     then
         cd ./vendor/citation-style-language/$1
-        git fetch origin master
         git pull origin master
     else
         git clone --branch=master https://github.com/citation-style-language/$1.git vendor/citation-style-language/$1
